e1da9ccd9ce65bd36bfd3a5647c4d908dfe7f1fa
MariiaKpln <kpln.mary@gmail.com>
2025-02-22 22:00:28 +0200

    correction if if else

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-function orderedList(array) {
-  let items = array.map((el) => isNum(el)).join(" ");
-  return `<ol>${items}</ol>`;
-}
-
-function isNum(el) {
-  let res = `<li class="item">${el}</li>`;
-  if (typeof el === "number") {
-    res = `<li class="item item_number">${el}</li>`;
+let array = ["HELLO", 122, -10, "Java", "JavaScript", 500, "Nodejs"];
+function compNumStr(e1, e2) {
+  const isE1Number = typeof e1 === "number";
+  const isE2Number = typeof e2 === "number";
+  let result;
+  if (isE1Number && isE2Number) {
+    result = compareNumbers(e1, e2);
+  } else if (!isE1Number && !isE2Number) {
+    result = comapareStr(e1, e2);
+  } else {
+    result = isE1Number ? -1 : 1;
   }
-  return res;
+  return result;
 }
 
-const bodyElement = document.querySelector("body");
-bodyElement.innerHTML = orderedList(["Hello", 300, "Java", "Nodejs", 100]);
-orderedList(bodyElement.innerHTML);
+function compareNumbers(a, b) {
+  return b - a;
+}
+
+function comapareStr(str1, str2) {
+  let result = 0;
+  if (str1 < str2) {
+    result = -1;
+  }
+  if (str1 > str2) {
+    result = 1;
+  }
+  return result;
+}
+
+const sorted = array.sort(compNumStr);
+console.log(sorted);
