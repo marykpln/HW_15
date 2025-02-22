a459a7574fa5747d7db9fe8944a129f5a36fddc5
MariiaKpln <kpln.mary@gmail.com>
2025-02-22 21:01:25 +0200

    second task is done!

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-let array = ["HELLO", 122, -10, "Java", "JavaScript", 500, "Nodejs"];
-function compNumStr(e1, e2) {
-  const isE1Number = typeof e1 === "number";
-  const isE2Number = typeof e2 === "number";
-  let result;
-  if (isE1Number && isE2Number) {
-    result = compareNumbers(e1, e2);
-  } else if (!isE1Number && !isE2Number) {
-    result = comapareStr(e1, e2);
-  }
-  return (result = isE1Number ? -1 : 1);
+function orderedList(array) {
+  let items = array.map((el) => isNum(el)).join(" ");
+  return `<ol>${items}</ol>`;
 }
 
-function compareNumbers(a, b) {
-  return b - a;
+function isNum(el) {
+  let res = `<li class="item">${el}</li>`;
+  if (typeof el === "number") {
+    res = `<li class="item item_number">${el}</li>`;
+  }
+  return res;
 }
 
-function comapareStr(str1, str2) {
-  let result = 0;
-  if (str1 < str2) {
-    result = -1;
-  }
-  if (str1 > str2) {
-    result = 1;
-  }
-  return result;
-}
-
-const sorted = array.sort(compNumStr);
-console.log(sorted);
-
-// [500, 122, -10, 'HELLO', 'Java', 'JavaScript','Nodejs']
+const bodyElement = document.querySelector("body");
+bodyElement.innerHTML = orderedList(["Hello", 300, "Java", "Nodejs", 100]);
+orderedList(bodyElement.innerHTML);
